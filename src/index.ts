68b9b31b6fa7a6e61fb76b3e240cd87a8e3c8d7d
Stas Dudzik <stas.dudzik@gmail.com>
2023-05-11 14:46:39 +0200

    ts tuples

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,10 @@
-let numbers: number[] = [1, 2, 3];
-let anyArray = [];
-// intellisense! when ts knows it's array of numbers it will suggest using number methods!
-numbers.forEach(n => n.toFixed())
-// if array is of type any as empty array with no specified type - no suggestions will be given
-anyArray.forEach(n=> n.)
+// ts will detect we cannot give 3 elements to 2 element tuple
+// let user: [number, string] = [1, "Staszek", 0];
+
+let user: [number, string] = [1, "Staszek"];
+//intellisense will suggest methods for different types in a tuple depending on index!
+// user[0].
+user[1];
+
+// ts tuple type compiles to regular js Array
+// its a good idea to restrict tuples to 2 elements in general in JS
