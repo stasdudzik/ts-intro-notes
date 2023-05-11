f921333af3d1ed315799500e0f068dba84bcbd8a
Stas Dudzik <stas.dudzik@gmail.com>
2023-05-11 14:42:18 +0200

    typing arrays

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,6 @@
-let sales = 123_456_789;
-let course = "TypeScript";
-let is_published = true;
-let level;
+let numbers: number[] = [1, 2, 3];
+let anyArray = [];
+// intellisense! when ts knows it's array of numbers it will suggest using number methods!
+numbers.forEach(n => n.toFixed())
+// if array is of type any as empty array with no specified type - no suggestions will be given
+anyArray.forEach(n=> n.)
