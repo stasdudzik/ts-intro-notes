18fd1d533f0a312200b66fb3693eee059895911b
Stas Dudzik <stas.dudzik@gmail.com>
2023-05-11 14:20:14 +0200

    ts type inference

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-let sales: number = 123_456_789;
-let course: string = "TypeScript";
-let is_published: boolean = true;
+let sales = 123_456_789;
+let course = "TypeScript";
+let is_published = true;
 let level;
