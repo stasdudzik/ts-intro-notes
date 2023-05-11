fe18b7a3f556f2cdfca1e87a219c9af05b8c20ed
Stas Dudzik <stas.dudzik@gmail.com>
2023-05-11 15:18:54 +0200

    objects in ts

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,10 @@
-function calculateTax(income: number, taxYear = 2022): number {
-  //   let x; no unused locals
-  if (taxYear < 2022) {
-    return income * 1.2;
-  }
-  return income * 1.3;
-}
-
-calculateTax(10_000, 2022);
-calculateTax(10_000);
+let employee: {
+  readonly id: number;
+  name: string;
+  retire: (date: Date) => void;
+} = {
+  id: 1,
+  name: "Staszek",
+  retire: (date: Date) => console.log(date),
+};
+// employee.id = 453; readonly property does not allow reassigning this property
