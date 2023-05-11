9e9c7a5282e69dc69d9205b141446ea0770dc2f9
Stas Dudzik <stas.dudzik@gmail.com>
2023-05-11 14:19:11 +0200

    ts primitive types declaration

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-console.log("Hello world");
-let age: number = 20;
-if (age < 50) age += 10;
-console.log(age);
+let sales: number = 123_456_789;
+let course: string = "TypeScript";
+let is_published: boolean = true;
+let level;
