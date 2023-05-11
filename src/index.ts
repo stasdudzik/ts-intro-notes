7daa733a962d25c611811bb4c13e64454c2be6ba
Stas Dudzik <stas.dudzik@gmail.com>
2023-05-11 15:40:42 +0200

    optional chaining with TS

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,24 @@
-function greet(name: string | undefined | null) {
-  if (name) {
-    console.log(name.toUpperCase());
-  } else {
-    console.log("HOLA!");
-  }
+type Customer = {
+  birthday?: Date;
+};
+
+function getCustomer(id: number): Customer | null | undefined {
+  return id === 0 ? null : { birthday: new Date() };
 }
-greet(null);
-greet("Piotr");
-greet(undefined);
+
+let customer = getCustomer(1);
+// if (customer !== null && customer !== undefined) {
+//   console.log(customer.birthday);
+// }
+
+//better use OPTIONAL PROPERTY ACCESS OPERATOR
+console.log(customer?.birthday?.getFullYear());
+
+// Optional element access operator for accessing array elements
+// customers[0]
+// customers?.[0]
+
+// Optional call - only execute function if its different then null or undefined!
+
+let log: any = null;
+log?.("a");
